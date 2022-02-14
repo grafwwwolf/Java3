65626334671f6896d539b4ff0d85721f3924b6b7
Stanislav <grafwwwolf@yandex.ru>
2022-02-14 20:27:33 +0300

    HW7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
+import testClasses.FirstTestClass;
+import testrunners.TestRunner;
 
+import java.lang.reflect.InvocationTargetException;
 
 public class Main {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException, InstantiationException, ClassNotFoundException {
+
+        TestRunner.start(FirstTestClass.class);
+        TestRunner.start("testClasses.FirstTestClass");
     }
 }
 
